d83c599a1ee85750ff8ff200793903b251fa7468
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2019-09-26 13:07:18 +0200

    edit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test");
+	
+    public static void main(String[] args) throws IOException{
+    	FileReader fr = new FileReader("data.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String jsonString = "";
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = new JSONObject(jsonString);
+
+        String zeile = "";
+
+        while( (zeile = br.readLine()) != null )
+        {
+        	jsonString = jsonString + zeile;
+        }
+        System.out.println(jsonString);
+        br.close();
     }
 }
