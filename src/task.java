19fa8bb39a8c4b253cd1d62da1b3ada627ac9e04
ChristinaBuenn <48588148+ChristinaBuenn@users.noreply.github.com>
2019-09-26 11:02:19 +0200

    Create task.java

---
+++ src/task.java
@@ -0,0 +1,31 @@
+
+public class task {
+	
+	 private String _description;
+	  private boolean _checked;
+	  private String _title;
+	  
+	  
+	public String get_description() {
+		return _description;
+	}
+	public void set_description(String _description) {
+		this._description = _description;
+	}
+
+	public boolean is_checked() {
+		return _checked;
+	}
+	public void set_checked(boolean _checked) {
+		this._checked = _checked;
+	}
+	public String get_title() {
+		return _title;
+	}
+	public void set_title(String _title) {
+		this._title = _title;
+	}
+	
+	   
+
+}
