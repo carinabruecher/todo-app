b5f507429da658a0ff8e481e4be40debba6cdf80
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2019-09-26 11:19:34 +0200

    changes name

---
+++ src/ITodoRepo.java
@@ -0,0 +1,3 @@
+public interface ITodoRepo {
+    public ArrayList<Task> getToDos();
+}
---
+++ src/TodoRepoTest.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class TodoRepoTest implements ITodoRepo {
+    private ArrayList<Task> _todos = new ArrayList<Task>();
+    public ArrayList<Task> getToDos(){
+        return _todos;
+    };
+
+
+}
